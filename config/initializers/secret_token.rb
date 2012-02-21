c73d608a95d7f82c630e34e6126a40febe0db8ba
chinacheng <chinachengliwen@gmail.com>
2012-02-21 21:23:02 +0800

    init too

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MongodbProject::Application.config.secret_token = 'cb3185aa0a5948e41bcb98a9bea8e17579f948731e737317c2910eb8b79dda5ef4cbf4af7bf88b59fe2d17f08725357526632efca268efd2458c145225d55068'
+MongodbProject::Application.config.secret_token = 'b5b35ee044ae95d5bff24b07f344e10f26056117131bc0857e118dc9c6bd0ca9a646a614b8f8159beb7d1ccc3b1603d63502697a911730c3595ce2a010b6dba7'
