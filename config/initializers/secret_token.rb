299b04fa959ae8fa33964bc76ea2b560088a9164
chinacheng <chinachengliwen@gmail.com>
2012-02-21 20:38:16 +0800

    first commit for rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongodbProject::Application.config.secret_token = 'cb3185aa0a5948e41bcb98a9bea8e17579f948731e737317c2910eb8b79dda5ef4cbf4af7bf88b59fe2d17f08725357526632efca268efd2458c145225d55068'
